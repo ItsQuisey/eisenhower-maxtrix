0569d47264f1cffd8ff5700dd9dbc26f6b6198e3
Marquise Stewart <113556700+ItsQuisey@users.noreply.github.com>
2025-08-15 20:27:24 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.getElementById("alertButton");
+    button.addEventListener("click", () => {
+        alert("Button was clicked! Let's prioritize wisely.");
+    });
+});
